ebc1f2a0faca324537e06ea63f4c207a7e4c29e1
Animan Amit <36138723+animanamit@users.noreply.github.com>
2025-05-20 16:39:54 +0530

    move to home page

--- app/routes.ts
+++ app/routes.ts
@@ -3,7 +3,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   // Product routes
-  route("products", "routes/products.tsx"),
+  // route("products", "routes/products.tsx"),
   // { path: "products/:id", lazy: () => import("./routes/products/detail.tsx") },
   // Inventory routes
   // { path: "inventory", lazy: () => import("./routes/inventory/index.tsx") },
